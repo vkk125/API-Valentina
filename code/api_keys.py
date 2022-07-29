571e6e1c12c6bfc30d5863626c6a84860124a9a4
vkk125 <vbaegzh@gmail.com>
2022-07-29 20:47:53 +0200

    finishedversion

--- code/api_keys.py
+++ code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "072622"
+weather_api_key = "4617a2fe24538369a55c642ba1f6f4ee"
 
 # Google API Key
-g_key = "072622"
+g_key = "AIzaSyBsTBqcSqARhZ8d7DNrglGkr0LtU0XzI4Q"
